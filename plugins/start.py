a911a85afbebef60d0c71c9afca6065596f36352
jaephefd12 <151659221+jaephefd12@users.noreply.github.com>
2023-11-22 20:11:13 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -88,8 +88,8 @@ async def start_command(client: Client, message: Message):
         reply_markup = InlineKeyboardMarkup(
             [
                 [
-                    InlineKeyboardButton("😊 About Me", callback_data = "about"),
-                    InlineKeyboardButton("🔒 Close", callback_data = "close")
+                    InlineKeyboardButton("AnimeGogo", url='https://telegram.me/animegogo'),
+                    InlineKeyboardButton("AnimeGogoNetwork", url='https://telegram.me/animegogonetork')
                 ]
             ]
         )
