a873bc81671880eacbf8b9acf5d3c99eac27da9f
jaephefd12 <151659221+jaephefd12@users.noreply.github.com>
2023-11-22 20:34:10 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -94,7 +94,20 @@ async def start_command(client: Client, message: Message):
             ]
         )
 
-        await message.reply_photo(caption="ffeffe",photo="https://miro.medium.com/v2/resize:fit:700/1*rh9WHM1FvhNHPqQavAirlQ.jpeg")
+        await message.reply_photo( caption = START_MSG.format(
+                first = message.from_user.first_name,
+                last = message.from_user.last_name,
+                username = None if not message.from_user.username else '@' + message.from_user.username,
+                mention = message.from_user.mention,
+                id = message.from_user.id
+            ),
+            reply_markup = reply_markup,
+            disable_web_page_preview = True,
+            quote = True ,
+            photo="https://miro.medium.com/v2/resize:fit:700/1*rh9WHM1FvhNHPqQavAirlQ.jpeg"
+        )
+
+        
         await message.reply_text(
             text = START_MSG.format(
                 first = message.from_user.first_name,
